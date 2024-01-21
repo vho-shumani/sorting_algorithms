094adee09bb0e9868ad7c73036708488b070f9c3
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-21 11:49:44 +0200

    updated 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,26 +7,26 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, x;
-    int min;
+	size_t i, j, x;
+	int min;
 
-    for (i = 0; i < size; i++)
-    {
-        x = i;
-        min = array[i];
-        
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[i])
-            {
-                array[i] = array[j];
-                x = j;
-            }
-        }
-        if (x != i && i != size - 1)
-        {
-            array[x] = min;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		x = i;
+		min = array[i];
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[i])
+			{
+				array[i] = array[j];
+				x = j;
+			}
+		}
+		if (x != i && i != size - 1)
+		{
+			array[x] = min;
+			print_array(array, size);
+		}
+	}
 }
