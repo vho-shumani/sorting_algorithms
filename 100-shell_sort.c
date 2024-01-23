96c84b1eb6ad3704e1a4caf04f71fe64be8cc017
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-23 19:23:38 +0200

    created 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * swap - switches two elements in array.
+ * @array: array.
+ * @index_1: index of first element.
+ * @index_2: index of second element. 
+*/
+void swap(int *a, int index_1, int index_2)
+{
+	int x;
+	x = a[index_1];
+	a[index_1] = a[index_2];
+	a[index_2] = x;
+}
+/**
+ * shell_sort: sorts array using shell sorting.
+ * @array: array.
+ * @size: size of array. 
+*/
+void shell_sort(int *array, size_t size)
+{
+	int k = 1, i, x, j;
+
+	while (k < (int)size)
+	{
+		k = k * 3 + 1;
+	}
+	k = (k - 1) / 3;
+	while (k > 0)
+	{
+		for (x = 0, i = k; i < (int)size && x < (int)size; i++, x++)
+		{
+			if (array[x] > array[i])
+			{
+				swap(array, x, i);
+				j = x;
+				while (array[j - k] > array[j])
+				{
+					swap(array, j - k, j);
+					j--;
+				}
+			}
+		}
+		print_array(array, size);
+		k = (k - 1) / 3;
+	}
+}
